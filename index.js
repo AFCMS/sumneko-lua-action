7cd628aef61f7975ea78a017eaaf7c9a13bf3581
AFCMS <afcm.contact@gmail.com>
2022-11-10 12:05:41 +0100

    Fixes

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 import core from "@actions/core";
 import github from "@actions/github";
+import exec from "@actions/exec";
+import { HttpClient } from "@actions/http-client";
+import fs from "fs";
 
 async function run() {
   try {
     const level = core.getInput("level", { required: true });
+    const version = core.getInput("version", { required: true });
+
     console.log(`Hello ${level}!`);
+
+    var http = new HttpClient();
+
+    core.info("Fetching Language Server...");
+
+    const file_path = "./language-server.tar.gz";
+
+    const file = fs.createWriteStream(file_path);
+
+    const response = await http.get(
+      `https://github.com/sumneko/lua-language-server/releases/download/${version}/lua-language-server-${version}-linux-x64.tar.gz`
+    );
+
+    if (response.message.statusCode !== 200) {
+      core.setFailed(
+        `Failed to download language server: ${response.message.statusCode}`
+      );
+      return;
+    }
+
+    try {
+      response.message.pipe(file);
+    } catch (error) {
+      core.setFailed(`Failed to download language server: ${error}`);
+      return;
+    }
+
+    exec.exec("tar", ["-xzf", file_path, "-C", "/tmp/lua-language-server"]);
 
     if (level === "World") {
     }
